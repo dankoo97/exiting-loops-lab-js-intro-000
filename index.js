ea014124b44aef4c3eb3befe5e91ec25142fe440
Daniel A Koontz <dak.7997@gmail.com>
2020-06-15 00:07:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function breakOut(array, changeValue, stopValue) {
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === stopValue) {
+    if (array[i] === skipValue) {
       continue
     } else {
       array[i] = changeValue
