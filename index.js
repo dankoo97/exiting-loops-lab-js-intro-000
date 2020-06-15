45651db6cf4febc22b54004dd3ccdfb37bb896e7
Daniel A Koontz <dak.7997@gmail.com>
2020-06-15 00:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i ++) {
     if (array[i] === findFn) {
+      console.log(array[i])
       return array[i]
     }
   }
