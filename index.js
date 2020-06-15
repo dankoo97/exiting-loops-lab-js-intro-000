1a9c54b95df822be46cf87d02bdf0927841b5c84
Daniel A Koontz <dak.7997@gmail.com>
2020-06-15 00:06:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function breakOut(array, changeValue, stopValue) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      return array
+    } else {
+      array[i] = changeValue
+    }
+  }
 }
